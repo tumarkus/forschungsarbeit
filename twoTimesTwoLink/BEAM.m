91caeb884070f5c684e6208f6a03cc3cc9ba658d
Tobias Keute <tobias.keute@tu-dortmund.de>
2013-10-11 23:33:15 +0530

    figs vom BEAM prog mit 2,5 und 10 elementen hinzugef.

--- twoTimesTwoLink/BEAM.m
+++ twoTimesTwoLink/BEAM.m
@@ -1,4 +1,4 @@
-function [ erg ] = BEAM( number, E, I, L, q0)
+function [ erg] = BEAM( number, E, I, L, q0)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 l=L/number;
@@ -27,4 +27,6 @@ y=zeros(number+1, 1);
 for i=1:number+1
     y(i)=v_vek(2*i-1);
 end
+
+
 erg=[x, y];
